0d95d46523d149c709319c654e8d97258a814a60
dhiraj-inti <intidhiraj3103@gmail.com>
2023-03-20 16:16:54 +0530

    blockchain update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useEffect,useState } from 'react';
 import './App.css';
 import Web3 from 'web3';
+import Appointments from './abi/Appointments.json'
 
 function App() {
   const ethereum = window.ethereum;
   let web3 = window.web3;
   const [account, setaccount] = useState("");
   const [netId, setNetId] = useState()
+  const [contract, setContract] = useState(null);
 
   useEffect(() => {
 
@@ -14,6 +16,20 @@ function App() {
       const acc = await web3.eth.getAccounts();
       setaccount(acc[0]);
       setNetId(await web3.eth.net.getId());
+      const networkData = Appointments.networks[netId];
+      if (networkData) {
+        console.log("Network data", networkData);
+        const abi = Appointments.abi;
+        const address = networkData.address;
+        console.log("Abi", abi);
+        console.log("Address", address);
+        const _contract = new web3.eth.Contract(abi, address);
+        //console.log("Obtained contract", _contract);
+        setContract(_contract);
+        console.log(contract)
+      } else {
+        window.alert("Smart contract not deployed to detected network");
+      }
     }
   
     async function loadWeb3() {
@@ -27,8 +43,15 @@ function App() {
         window.alert('Please use metamask')
       }
     }
+
+    async function bookAnAppointment(){
+      //const result = await contract.methods.getAllAppointments().call();
+      console.log(contract.methods)
+    }
     loadWeb3();
     loadBlockchainData();
+    bookAnAppointment();
+    // fetchDataFromBlockchain();
     // eslint-disable-next-line
   }, [])
 
