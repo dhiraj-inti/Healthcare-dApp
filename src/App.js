bcb763c2d7c4841a33b89845e3601f1f36522ab1
dhiraj-inti <intidhiraj3103@gmail.com>
2023-03-21 21:48:45 +0530

    update in frontend

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect,useState } from 'react';
 import './App.css';
 import Web3 from 'web3';
 import Appointments from './abi/Appointments.json'
+import DrugInventory from './abi/DrugInventory.json'
 
 function App() {
   const ethereum = window.ethereum;
@@ -9,6 +10,9 @@ function App() {
   const [account, setaccount] = useState("");
   const [netId, setNetId] = useState()
   const [contract, setContract] = useState(null);
+  const [DIContract, setDIContract] = useState(null);
+  const [res, setResp] = useState([])
+  const [drugRes, setDrugRes] = useState([])
   
 
   useEffect(() => {
@@ -26,10 +30,35 @@ function App() {
         const x = await web3.eth.net.getId()
         setNetId(x)
         const networkData = Appointments.networks[x];
+        const networkDataDI = DrugInventory.networks[x];
         const t_contract = new web3.eth.Contract(Appointments.abi, networkData.address);
+        const DI_contract = new web3.eth.Contract(DrugInventory.abi, networkDataDI.address);
+        setDIContract(DI_contract);
         setContract(t_contract);
         const resp = await t_contract.methods.getAllAppointments().call()
-        console.log(resp)
+        const respDI = await DI_contract.methods.getAllReceipts().call()
+        let t = []
+        let DI = []
+        for(let i=0;i<resp.length;++i){
+          t.push(JSON.stringify({
+            "Doctor":resp[i].doctor,
+            "Patient":resp[i].patient,
+            "Slot Number":resp[i].slotNo,
+            "Timestamp":resp[i].timestamp
+          }))
+        }
+
+        for(let i=0;i<respDI.length;++i){
+          DI.push(JSON.stringify({
+            "Doctor ID":respDI[i].doctorId,
+            "Patient":respDI[i].patientName,
+            "Pharmacist":respDI[i].pharmaId,
+            "Medicines":respDI[i].medicines,
+            "Timestamp":respDI[i].timestamp
+          }))
+        }
+        setResp(t);
+        setDrugRes(DI);
       } else if (web3) {
         web3 = new Web3(web3.currentProvider);
       } else {
@@ -39,15 +68,34 @@ function App() {
 
     loadWeb3()
     loadBlockchainData()
-    
-    // fetchDataFromBlockchain();
     // eslint-disable-next-line
   }, [])
 
 
   return (
     <div className="App">
-      Hello this is Helathcare dApp {account} {netId}
+      <h3>Account used on this page is : {account}</h3> 
+      <div>
+        <h1>Appointments Details</h1>
+        {res.map((ele)=>{
+          return (
+            <div>
+              {ele}
+            </div>
+          )
+        })}
+      </div>
+
+      <div>
+        <h1>Drug Inventory Details</h1>
+        {drugRes.map((ele)=>{
+          return (
+            <div>
+              {ele}
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
